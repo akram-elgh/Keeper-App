cff02eebc4ad3b4e85cc1acb14002de7beeec451
Akram-Elghalbzouri <elghalbzouri.akram@gmail.com>
2023-02-23 15:06:45 +0100

    Adding RESTFUl api - Adding post route and saving data to database - Adding delete route and deleting data from database

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,22 +6,42 @@ import Footer from "./Footer";
 import Input from "./Input";
 function App() {
   const [notes, setNotes] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:3001/api")
       .then((response) => response.json())
       .then((data) => setNotes(data));
   }, []);
+
   function addNote(newNote) {
-    setNotes((prevValue) => {
-      return [...prevValue, { title: newNote.title, body: newNote.body }];
+    fetch("http://localhost:3001/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newNote),
+    }).then(() => {
+      fetch("http://localhost:3001/api")
+        .then((response) => response.json())
+        .then((data) => setNotes(data));
     });
+    // setNotes((prevValue) => {
+    //   return [...prevValue, { title: newNote.title, body: newNote.body }];
+    // });
   }
   function deleteNote(id) {
-    setNotes((prevValue) => {
-      return prevValue.filter((note, index) => {
-        return index !== id;
-      });
+    fetch(`http://localhost:3001/api?id=${id}`, {
+      method: "DELETE",
+    }).then(() => {
+      fetch("http://localhost:3001/api")
+        .then((response) => response.json())
+        .then((data) => setNotes(data));
     });
+    // setNotes((prevValue) => {
+    //   return prevValue.filter((note, index) => {
+    //     return index !== id;
+    //   });
+    // });
   }
   return (
     <div>
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors({ origin: "http://localhost:3000" }));
+app.use(bodyParser.json());
 mongoose.set("strictQuery", false);
 // Connecting to mongodb databse at port 27017
 mongoose.connect("mongodb://localhost:27017/notesDB", (err) => {
@@ -19,11 +19,32 @@ const NoteSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", NoteSchema);
 
-app.route("/api").get((req, res) => {
-  Note.find((err, data) => {
-    res.json(data || []);
+app
+  .route("/api")
+  .get((req, res) => {
+    Note.find((err, data) => {
+      res.json(data || []);
+    });
+  })
+  .post((req, res) => {
+    const { title, body } = req.body;
+    const note = new Note({
+      title: title,
+      body: body,
+    });
+    note.save();
+    // Note.find((err, data) => {
+    //   console.log(data);
+    //   res.json(data || []);
+    // });
+    res.sendStatus(200);
+  })
+  .delete((req, res) => {
+    Note.findByIdAndDelete(req.query.id, (err, data) => {
+      console.log(data);
+      res.sendStatus(200);
+    });
   });
-});
 
 app.listen(3001, (err) => {
   console.log(err || "Server started on port 3001");
